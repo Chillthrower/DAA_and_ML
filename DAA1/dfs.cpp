d17dd59cdb8d16d291fc89b04cc03e864d6f9582
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2024-04-07 21:18:42 +0530

    Update dfs.cpp

--- DAA1/dfs.cpp
+++ DAA1/dfs.cpp
@@ -1,55 +1,69 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-#define MAX_VERTICES 6
+#define MAX_VERTICES 100
 
-int adjacencyMatrix[MAX_VERTICES][MAX_VERTICES] = 
+void dfs(int v, int a[MAX_VERTICES][MAX_VERTICES], int visited[], int vertex) 
 {
-    {0, 1, 1, 0, 0, 0},
-    {1, 0, 0, 1, 1, 0},
-    {1, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 1},
-    {0, 1, 1, 0, 0, 1},
-    {0, 0, 0, 1, 1, 0}
-};
+    visited[vertex] = 1; 
 
-int visited[MAX_VERTICES] = {0};
-
-void dfs(int vertex) 
-{
-    visited[vertex] = 1;
-    for (int i = 0; i < MAX_VERTICES; i++) 
+    for (int i = 0; i < v; i++) 
     {
-        if (adjacencyMatrix[vertex][i] && !visited[i]) 
-        {
-            dfs(i);
+        if (a[vertex][i] && !visited[i]) 
+        { 
+            dfs(v, a, visited, i); 
         }
     }
-}
-
-bool isConnected() 
-{
-    dfs(0);
-    for (int i = 0; i < MAX_VERTICES; i++) 
-    {
-        if (!visited[i]) 
-        {
-            return false;
-        }
-    }
-    return true;
 }
 
 int main() 
 {
-    if (isConnected()) 
+    int v;
+    printf("Enter the number of vertices:\n");
+    scanf("%d", &v);
+
+    int a[MAX_VERTICES][MAX_VERTICES];
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < v; i++) 
     {
-        printf("The graph is connected.\n");
-    } 
-    else 
+        for (int j = 0; j < v; j++) 
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int visited[MAX_VERTICES] = {0}; 
+
+    dfs(v, a, visited, 0);
+
+    int connected = 1;
+    for (int i = 0; i < v; i++) 
     {
-        printf("The graph is not connected.\n");
+        if (!visited[i]) 
+        {
+            connected = 0;
+            break;
+        }
+    }
+
+    if (connected) 
+    {
+        printf("The graph is connected\n");
+    } else 
+    {
+        printf("The graph is disconnected\n");
     }
 
     return 0;
 }
+
+// sample input
+
+// Enter the number of vertices:
+// 5
+// Enter the adjacency matrix:
+// 0 1 0 0 0
+// 1 0 0 0 0
+// 0 0 0 1 0
+// 0 0 1 0 0
+// 0 0 0 0 0
