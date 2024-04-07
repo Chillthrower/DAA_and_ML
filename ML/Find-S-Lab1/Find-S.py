2195651d9d143d7adc1d7d2be75decbb9fa7578c
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2024-04-07 12:20:06 +0530

    Update Find-S.py

--- ML/Find-S-Lab1/Find-S.py
+++ ML/Find-S-Lab1/Find-S.py
@@ -1,6 +1,4 @@
-# Implement and demonstrate the FIND-S algorithm for finding the most specific hypothesis
-# based on a given set of training data samples. Read the training data from a .CSV file
-
+# without redused code
 import pandas as pd
 
 data = pd.read_csv('enjoysport.csv')
@@ -15,6 +13,21 @@ for example in positive_examples:
             hypothesis[i] = '?'
         else:
             hypothesis[i] = example[i]
+
+print("The maximally specific Find-S hypothesis for the given training examples is:")
+print(hypothesis)
+
+# reduced code
+import pandas as pd
+
+data = pd.read_csv('enjoysport.csv')
+
+hypothesis = ['%' for _ in range(len(data.columns) - 1)]
+
+positive_examples = data[data['EnjoySport'] == 'Yes'].iloc[:, :-1].values.tolist()
+
+for example in positive_examples:
+    hypothesis = [example[i] if hypothesis[i] == '%' or hypothesis[i] == example[i] else '?' for i in range(len(example))]
 
 print("The maximally specific Find-S hypothesis for the given training examples is:")
 print(hypothesis)
