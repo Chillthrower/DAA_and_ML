7079a570c301fe5068ddcce0a7b7409218e29120
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2024-04-07 21:18:57 +0530

    Update bfs.cpp

--- DAA1/bfs.cpp
+++ DAA1/bfs.cpp
@@ -1,42 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_VERTICES 6
+#define MAX_VERTICES 100
 
-int adjacencyMatrix[MAX_VERTICES][MAX_VERTICES] = 
+void bfs(int source, int n, int a[MAX_VERTICES][MAX_VERTICES], int s[MAX_VERTICES]) 
 {
-    {0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0},
-    {0, 0, 0, 0, 1, 0},
-    {0, 0, 0, 0, 0, 1},
-    {0, 0, 0, 0, 0, 1},
-    {0, 0, 0, 0, 0, 0}
-};
-
-void bfs(int start) {
-    int queue[MAX_VERTICES], front = 0, rear = -1;
-    int visited[MAX_VERTICES] = {0};
-    printf("Nodes reachable from node %d: %d ", start, start);
-    visited[start] = 1;
-    queue[++rear] = start;
-
-    while (front <= rear) 
+    int f = 1, r = 1, v, u, q[MAX_VERTICES];
+    q[r] = source;
+    s[source] = 1;
+    while (f <= r) 
     {
-        int currentVertex = queue[front++];
-        for (int i = 0; i < MAX_VERTICES; i++) 
+        u = q[f++];
+        for (v = 0; v < n; v++) 
         {
-            if (adjacencyMatrix[currentVertex][i] && !visited[i]) 
+            if (s[v] == 0 && a[u][v] == 1) 
             {
-                visited[i] = 1;
-                printf("%d ", i);
-                queue[++rear] = i;
+                q[++r] = v;
+                s[v] = 1;
             }
         }
     }
-    printf("\n");
 }
 
 int main() 
 {
-    bfs(1);
+    int v;
+    printf("Enter the number of vertices:\n");
+    scanf("%d", &v);
+
+    int a[MAX_VERTICES][MAX_VERTICES];
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < v; i++) 
+    {
+        for (int j = 0; j < v; j++) 
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int s[MAX_VERTICES] = {0}; 
+
+    int source;
+    printf("Enter the source vertex:\n");
+    scanf("%d", &source);
+
+    bfs(source, v, a, s);
+
+    printf("BFS Reachability:\n");
+    for (int i = 0; i < v; i++) 
+    {
+        if (s[i] == 1) 
+        {
+            printf("%d is reachable from %d\n", i, source);
+        } else 
+        {
+            printf("%d is not reachable from %d\n", i, source);
+        }
+    }
+
     return 0;
 }
+
+// sample input
+
+// Enter the number of vertices:
+// 5
+// Enter the adjacency matrix:
+// 0 1 0 0 0
+// 1 0 0 0 0
+// 0 0 0 1 0
+// 0 0 1 0 0
+// 0 0 0 0 0
+// Enter the source vertex:
+// 0
