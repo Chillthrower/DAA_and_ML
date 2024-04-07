11fc7783c571c3d626036e15c65fa32337affbbc
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2024-04-07 15:17:43 +0530

    Update ID3.py

--- ML/ID3-Lab3/ID3.py
+++ ML/ID3-Lab3/ID3.py
@@ -102,3 +102,94 @@ def print_tree(node, level):
 metadata, traindata = read_data("tennisdata.csv")
 node = create_node(traindata, metadata)
 print_tree(node, 0)
+
+# reduced code
+
+import numpy as np
+import math
+import csv
+
+def read_data(filename):
+    with open(filename, 'r') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=',')
+        metadata = [name for name in next(datareader)]
+        traindata = np.array([row for row in datareader])
+    return metadata, traindata
+
+class Node:
+    def __init__(self, attribute):
+        self.attribute = attribute
+        self.children = []
+        self.answer = ""
+
+    def __str__(self):
+        return self.attribute
+
+    def is_leaf(self):
+        return self.answer != ""
+
+def subtables(data, col, delete):
+    items = np.unique(data[:, col])
+    dict = {
+        item: np.array([row for row in data if row[col] == item])
+        for item in items
+    }
+    if delete:
+        dict = {
+            item: np.delete(table, col, axis=1)
+            for item, table in dict.items()
+        }
+    return items, dict
+
+def entropy(S):
+    items, counts = np.unique(S, return_counts=True)
+    if len(items) == 1:
+        return 0
+    probs = counts / len(S)
+    return -np.sum(probs * np.log2(probs))
+
+def gain_ratio(data, col):
+    items, dict = subtables(data, col, delete=False)
+    total_size = data.shape[0]
+    entropies = np.zeros((items.shape[0], 1))
+    intrinsic = np.zeros((items.shape[0], 1))
+    for x in range(items.shape[0]):
+        ratio = dict[items[x]].shape[0]/(total_size * 1.0)
+        entropies[x] = ratio * entropy(dict[items[x]][:, -1])
+        intrinsic[x] = ratio * math.log(ratio, 2)
+    total_entropy = entropy(data[:, -1])
+    iv = -1 * sum(intrinsic)
+    for x in range(entropies.shape[0]):
+        total_entropy -= entropies[x]
+    return total_entropy / iv
+
+def create_node(data, metadata):
+    if len(set(data[:, -1])) == 1:
+        node = Node("")
+        node.answer = data[0, -1]
+        return node
+    gains = np.array([gain_ratio(data, col) for col in range(data.shape[1] - 1)])
+    split = np.argmax(gains)
+    node = Node(metadata[split])
+    metadata = np.delete(metadata, split, 0)
+    items, dict = subtables(data, split, delete=True)
+    for x in range(items.shape[0]):
+        child = create_node(dict[items[x]], metadata)
+        node.children.append((items[x], child))
+    return node
+
+def empty(size):
+    return " " * size
+
+def print_tree(node, level):
+    if node.is_leaf():
+        print(empty(level), node.answer)
+    else:
+        print(empty(level), node.attribute)
+        for value, child in node.children:
+            print(empty(level + 1), value)
+            print_tree(child, level + 2)
+
+metadata, traindata = read_data("tennisdata.csv")
+node = create_node(traindata, metadata)
+print_tree(node, 0)
